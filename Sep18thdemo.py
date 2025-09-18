ed5b42855e060cb47c8c4c4e90573e89a855c4ab
tobinkael <kael.tobin@edu.sait.ca>
2025-09-18 15:55:49 -0600

    accidently commit

--- Sep18thdemo.py
+++ Sep18thdemo.py
@@ -18,3 +18,12 @@ print(a,type(a))
 print(b,type(b))
 print(c,type(c))
 print(d,type(d))
+
+# Some call functions
+
+# print
+# type
+# input
+# casting - if manual: EXPLICIT casting
+# promoting/demoting - the more precise or the
+# larger amount of memory allocated is "higher"
