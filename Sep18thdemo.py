565a454684c5543593e8b606b2f2de029bb1281d
tobinkael <kael.tobin@edu.sait.ca>
2025-09-18 15:44:05 -0600

    Git demo week 3, assignments

---
+++ Sep18thdemo.py
@@ -0,0 +1,20 @@
+#This is our third class demo.
+"""
+this is a multi line
+comment
+in this demo we will work on variables,
+operators and such
+"""
+
+# Review
+# Assignment
+
+a = 3
+b = 3.5
+c = True
+d = "whats crackin"
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
